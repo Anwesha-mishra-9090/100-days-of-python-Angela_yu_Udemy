7b628462a4b8ae8b6ed6022d8b40cc504da2f432
Anwesha mishra <mishra.anwesha143@gmail.com>
2025-10-24 13:05:43 +0530

    Delete bandname generator project directory

--- bandname generator project/band_name_generator.py
+++
@@ -1,5 +0,0 @@
-print("WELCOME TO BAND NAME GENERATOR.")
-city = input("what is the name of the city you grew up in ?\n")
-pet_name = input("what is your pet name?\n")
-print("your band name could be : " + city + pet_name )
-
